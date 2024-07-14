b1a4e4c74ea0cddd2f22bcb571ff61171a49de9c
Utsav kumar <152022032+utsavkumar2004@users.noreply.github.com>
2024-07-14 16:44:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+// • Task 1: Declare a variable using var, assign it a number, and log the value to the console.
+var myNumber = 69;
+console.log(myNumber);
+
+// • Task 2: Declare a variable using let, assign it a string, and log the value to the console.
+let myString1 = "Utsav Bhardwaj";
+console.log(myString1);
+
+// • Task 3: Declare a variable using const, assign it a boolean value, and log the value to the console.
+const myBoolean = false;
+console.log(myBoolean);
+
+// • Task 4: Create variables of different data types (number, string, boolean, object, array) and log each variable's type using the typeof operator.
+let myNumber = 69;
+let myString = "Hello, Brother";
+let myBoolean1 = true;
+let myObject = { name: "utsav bhardwaj" };
+let myArray = [5, 10, 15, 20, 25];
+
+console.log(typeof myNumber);
+console.log(typeof myString);
+console.log(typeof myBoolean1); 
+console.log(typeof myObject); 
+console.log(typeof myArray);
+
+// • Task 5: Declare a variable using let, assign it an initial value, reassign a new value, and log both values to the console.
+let myVariable = 10;
+console.log(myVariable); 
+
+myVariable = 20;
+console.log(myVariable);
+
+// • Task 6: Try reassigning a variable declared with const and observe the error.
+const myConstant = 100;
+console.log(myConstant); // Output: 100
+myConstant = 200;
+
